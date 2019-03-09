26f0b653285484725278c8cebfa29236af0a90b9
Chovin Carlson <Chovin@users.noreply.github.com>
2019-03-09 12:47:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,11 +5,11 @@ class Triangle
   end
   
   def kind
-    if sides.include? 0 || a+b<c || c+a<b || b+c<a then
+    if @sides.include? 0 || a+b<c || c+a<b || b+c<a then
       raise TriangleError
-    elsif sides.uniq.length == 1
+    elsif @sides.uniq.length == 1
       return :equilateral
-    elsif sides.uniq.lenght == 2
+    elsif @sides.uniq.lenght == 2
       return :isosceles
     end
     return :scalene
