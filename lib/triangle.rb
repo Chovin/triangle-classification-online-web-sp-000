2a7f01f8bec2dd802d855f7209d1bbb3e9b75cc1
Chovin Carlson <Chovin@users.noreply.github.com>
2019-03-09 12:52:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   
   def kind
     a,b,c = @sides
-    if (@sides.select {|x| x <= 0}).length>0 || a+b<c || c+a<b || b+c<a then
+    if (@sides.select {|x| x <= 0}).length>0 || a+b<=c || c+a<=b || b+c<=a then
       raise TriangleError
     elsif @sides.uniq.length == 1
       return :equilateral
